b09b25c916e4c30f9b254acaf07ba2b924b97675
adrian92612 <adrianvillamin0612@gmail.com>
2023-09-17 01:12:37 +0800

    added description section

--- src/home.js
+++ src/home.js
@@ -9,10 +9,27 @@ function createSteakImg() {
   return homeImg;
 }
 
+function createDescription() {
+  const descSection = document.createElement("div");
+  descSection.className = "desc-section";
+
+  const slogan = document.createElement("h1");
+  slogan.innerText = "Your Steak Sanctuary";
+
+  const description = document.createElement("p");
+  description.innerText =
+    "Experience culinary excellence at Amber's Steakhouse, nestled in the heart of Manila. Our warm, inviting ambiance and skilled chefs craft exceptional prime cuts to perfection, infusing each steak with unforgettable flavor. Whether celebrating a special occasion or savoring time with loved ones, our attentive staff ensures your experience exceeds expectations. With a meticulously curated menu and extensive wine selection, Amber's Steakhouse elevates every bite, creating lasting memories in a welcoming setting.";
+
+  descSection.appendChild(slogan);
+  descSection.appendChild(description);
+  return descSection;
+}
+
 export default function createHome() {
   const homePage = document.createElement("main");
   homePage.className = "home";
 
   homePage.appendChild(createSteakImg());
+  homePage.appendChild(createDescription());
   return homePage;
 }
