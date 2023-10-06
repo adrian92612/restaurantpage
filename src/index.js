8ce108ab300c2ac419eb8d509e0241090f490d8c
adrian92612 <adrianvillamin0612@gmail.com>
2023-10-06 11:30:23 +0800

    added svg icon close for popup menu

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import createFooter from "./footer";
 const root = document.getElementById("root");
 const mainContent = document.createElement("main");
 
-function popUpNav() {}
+function popUpNav() {
+  console.log("clicked");
+}
 
 function addListeners() {
   const navIcon = document.querySelector(".nav-icon");
-  navIcon.addEventListener("click", popUpNav());
+  navIcon.addEventListener("click", popUpNav);
 }
 
 (function init() {
